29cc6c5f8c0390d0f61438888d1e756df4dfefd7
Luke Emmet <luke.emmet@gmail.com>
2020-08-30 22:19:41 +0100

    first working version yay!

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
 # duckling-proxy
-Duckling proxy is a Gemini proxy to access the Small Web
+Duckling proxy is a Gemini proxy to access the Small Web. 
+
+Cross platform, written in Go.
+
+## What is the Small Web?
+
+The Small Web are those pages on the WWW that are typically characterised:
+
+* simple and document/content centric, using simple headings, bullets, links and tables only
+* accessible to different web clients, but do not need a monster browser such as Chrome to view them
+* do not require client side javascript
+* do not require tracking, cookies, or authentication
+* could be rendered as text/gemini without significant loss of information
+
+## What is the Duckling proxy?
+
+The Duckling proxy is a scheme-specific filtering proxy for Gemini clients to access the WWW. It behaves as a normal Gemini server, except it retrieves its content from the web. You can tailor its behaviour when it starts, to tailor how web pages are transformed to gemtext.
+
+It is scheme-specific, i.e. it is designed to handle HTTP requests only. Agena is another example of a scheme specific proxy, supporting <code>gopher://</code>
+
+Web pages are translated to text/gemini. Other web resources are returned directly.
+
+The primary intended use case for this proxy is as a personal proxy to make the web accessible to your favourite Gemini client. 
+
+## Usage
+
+```
+Usage:
+
+duckling-proxy [flags]
+
+  -m, --citationMarkers         Use footnote style citation markers
+  -s, --citationStart int       Start citations from this index (default 1)
+  -e, --emitImagesAsLinks       Emit links to included images
+  -l, --linkEmitFrequency int   Emit gathered links through the document after this number of paragraphs (default 2)
+  -T, --maxConnectTime int      Max connect time (s)
+                                 (default 5)
+  -t, --maxDownloadTime int     Max download time (s)
+                                 (default 10)
+  -n, --numberedLinks           Number the links
+  -p, --port int                Server port (default 1965)
+  -r, --prettyTables            Pretty tables - works with most simple tables
+  -c, --serverCert string       serverCert path.
+  -k, --serverKey string        serverKey path.
+  -u, --userAgent string        User agent for HTTP requests
+```
+
+## Remarks
+
+* serverCert - required - path to Gemini server TLS certificate
+* serverKey - required - path to Gemini server TLS private key
+* All other flags are optional and you can experiment with them
+
+You will need to configure your Gemini client to point to the server when there is a need to access any <code>http://</code> or <code>https://</code> requests.
+
+## Supported clients
+
+* GemiNaut (unreleased build, coming soon)
+* gemget
+* diohsc
+* AV-98 (adapt Agena handling code for HTTP/HTTPS)
+
+## Installation
+
+Retrieve the package using Go get
+
+```
+go get github.com/LukeEmmet/duckling-proxy
+```
+
+Build using Go build
+
+```
+go build github.com/LukeEmmet/duckling-proxy
+```
+
+## Feedback
+
+Send me your thoughts and feedback to
+
+```
+luke [at] marmaladefoo [dot] com
+```
+
---
+++ duckling-proxy.go
@@ -0,0 +1,199 @@
+package main
+
+import (
+	"github.com/LukeEmmet/html2gemini"
+	gemini "github.com/makeworld-the-better-one/go-gemini"
+	flag "github.com/spf13/pflag"
+
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+)
+
+type WebPipeHandler struct {
+}
+
+var (
+	citationStart     = flag.IntP("citationStart", "s", 1, "Start citations from this index")
+	citationMarkers   = flag.BoolP("citationMarkers", "m", false, "Use footnote style citation markers")
+	numberedLinks     = flag.BoolP("numberedLinks", "n", false, "Number the links")
+	prettyTables      = flag.BoolP("prettyTables", "r", false, "Pretty tables - works with most simple tables")
+	emitImagesAsLinks = flag.BoolP("emitImagesAsLinks", "e", false, "Emit links to included images")
+	linkEmitFrequency = flag.IntP("linkEmitFrequency", "l", 2, "Emit gathered links through the document after this number of paragraphs")
+	serverCert        = flag.StringP("serverCert", "c", "", "serverCert path. ")
+	serverKey         = flag.StringP("serverKey", "k", "", "serverKey path. ")
+	userAgent         = flag.StringP("userAgent", "u", "", "User agent for HTTP requests\n")
+	maxDownloadTime   = flag.IntP("maxDownloadTime", "t", 10, "Max download time (s)\n")
+	maxConnectTime    = flag.IntP("maxConnectTime", "T", 5, "Max connect time (s)\n")
+	port              = flag.IntP("port", "p", 1965, "Server port")
+)
+
+func fatal(format string, a ...interface{}) {
+	urlError(format, a...)
+	os.Exit(1)
+}
+
+func urlError(format string, a ...interface{}) {
+	format = "Error: " + strings.TrimRight(format, "\n") + "\n"
+	fmt.Fprintf(os.Stderr, format, a...)
+}
+
+func info(format string, a ...interface{}) {
+	format = "Info: " + strings.TrimRight(format, "\n") + "\n"
+	fmt.Fprintf(os.Stderr, format, a...)
+}
+
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+		os.Exit(1)
+	}
+}
+
+func htmlToGmi(inputHtml string) (string, error) {
+
+	//convert html to gmi
+	options := html2gemini.NewOptions()
+	options.PrettyTables = *prettyTables
+	options.CitationStart = *citationStart
+	options.LinkEmitFrequency = *linkEmitFrequency
+	options.CitationMarkers = *citationMarkers
+	options.NumberedLinks = *numberedLinks
+	options.EmitImagesAsLinks = *emitImagesAsLinks
+
+	//dont use an extra line to separate header from body, but
+	//do separate each row visually
+	options.PrettyTablesOptions.HeaderLine = false
+	options.PrettyTablesOptions.RowLine = true
+
+	//pretty tables option is somewhat experimental
+	//and the column positions not always correct
+	//so use invisible borders of spaces for now
+	options.PrettyTablesOptions.CenterSeparator = " "
+	options.PrettyTablesOptions.ColumnSeparator = " "
+	options.PrettyTablesOptions.RowSeparator = " "
+
+	ctx := html2gemini.NewTraverseContext(*options)
+
+	return html2gemini.FromString(inputHtml, *ctx)
+
+}
+
+
+func (h WebPipeHandler) Handle(r gemini.Request) *gemini.Response {
+
+	url := r.URL.String()
+	if r.URL.Scheme != "http" && r.URL.Scheme != "https" {
+		//any other schemes are not implemented by this proxy
+		return &gemini.Response{53, "Scheme not supported: " + r.URL.Scheme, nil, nil}
+	}
+
+	info("Retrieve: %s", r.URL.String())
+
+	//see https://medium.com/@nate510/don-t-use-go-s-default-http-client-4804cb19f779
+	//also https://gist.github.com/ijt/950790/fca88967337b9371bb6f7155f3304b3ccbf3946f
+
+	connectTimeout := time.Second * time.Duration(*maxConnectTime)
+	clientTimeout := time.Second * time.Duration(*maxDownloadTime)
+
+	//create custom transport with timeout
+	var netTransport = &http.Transport{
+		Dial: (&net.Dialer{
+			Timeout: connectTimeout,
+		}).Dial,
+		TLSHandshakeTimeout: connectTimeout,
+	}
+
+	//create custom client with timeout
+	var netClient = &http.Client{
+		Timeout:   clientTimeout,
+		Transport: netTransport,
+	}
+
+	//fmt.Println("making request")
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return &gemini.Response{43, "Could not connect to remote HTTP host", nil, nil}
+	}
+
+	//set user agent if specified
+	if *userAgent != "" {
+		req.Header.Add("User-Agent", *userAgent)
+	}
+
+	response, err := netClient.Do(req)
+	if err != nil {
+		return &gemini.Response{43, "Remote host did not respond with valid HTTP", nil, nil}
+	}
+
+
+	defer response.Body.Close()
+
+	//final response (may have redirected)
+	if url != response.Request.URL.String() {
+		//notify of target location on stderr
+		//see https://stackoverflow.com/questions/16784419/in-golang-how-to-determine-the-final-url-after-a-series-of-redirects
+		info("Redirected to: %s", response.Request.URL.String())
+
+		//tell the client to get it from a different location otherwise the client
+		//wont know the baseline for link refs
+		return &gemini.Response{30, response.Request.URL.String(), nil, nil}
+	}
+
+	contents, err := ioutil.ReadAll(response.Body)
+	check(err)
+
+	if response.StatusCode == 200 {
+		contentType := response.Header.Get("Content-Type")
+
+		info("Content-Type: %s", contentType)
+
+		var body io.ReadCloser
+		if strings.Contains(contentType, "text/html") {
+
+			info("Converting to text/gemini: %s", r.URL.String())
+
+			//translate html to gmi
+			gmi, err := htmlToGmi(string(contents))
+			
+			if err != nil {
+				return &gemini.Response{42, "HTML to GMI conversion failure", nil, nil}
+			}
+			
+			body = ioutil.NopCloser(strings.NewReader(string(gmi)))
+			contentType = "text/gemini"
+			
+		} else {
+			//let everything else through with the same content type
+			body = ioutil.NopCloser(strings.NewReader(string(contents)))
+		}
+
+		return &gemini.Response{20, contentType, body, nil}
+
+	} else if response.StatusCode == 404 {
+		return &gemini.Response{51, "Not found", nil, nil}
+	} else {
+		return &gemini.Response{50, "Failure: HTTP status: " + response.Status, nil, nil}
+	}
+
+}
+
+func main() {
+
+	flag.Parse()
+
+	handler := WebPipeHandler{}
+
+	info("Starting server on port: %d", *port)
+	err := gemini.ListenAndServe("", *serverCert, *serverKey, handler)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
